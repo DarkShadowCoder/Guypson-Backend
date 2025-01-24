f4d9cf09622afbb22531c8dd24ae50ed529545c9
Blackdot <106665251+DarkShadowCoder@users.noreply.github.com>
2025-01-24 16:29:38 +0100

    Update semencier.controller.js

--- controllers/Census/semencier.controller.js
+++ controllers/Census/semencier.controller.js
@@ -26,26 +26,46 @@ exports.getSemencierById = (req, res) => {
 // Créer un nouvel Semencier
 exports.createSemencier = (req, res) => {
   const data = req.body;
-  Semencier.create(data, (err, result) => {
-    if (err) {
-      res.status(500).send({ message: 'Erreur lors de la création de l’Semencier' });
-      return;
+  const nom = data.nom
+  try{
+    if(!nom){
+      res.status(403).send({message : "Nom du Drader requis !! "})
     }
-    res.status(201).json({ id: result.insertId, ...data });
-  });
+    else{
+      Semencier.create(data, (err, result) => {
+        if (err) {
+          res.status(500).send({ message: 'Erreur lors de la création de l’Semencier' });
+          return;
+        }
+        res.status(201).json({ id: result.insertId, ...data });
+      });
+    }
+  } catch (e) {
+    res.status(500).send({erreur: e})
+  }
 };
 
 // Mettre à jour un Semencier
 exports.updateSemencier = (req, res) => {
   const id = req.params.id;
   const data = req.body;
-  Semencier.update(id, data, (err, result) => {
-    if (err || result.affectedRows === 0) {
-      res.status(404).send({ message: 'Échec de la mise à jour de l’Semencier' });
-      return;
+  const nom = data.nom
+  try{
+    if(!nom){
+      res.status(403).send({message : "Nom du semencier requis !! "})
     }
-    res.status(200).json({ message: 'Semencier mis à jour avec succès' });
-  });
+    else{
+      Semencier.update(id, data, (err, result) => {
+        if (err || result.affectedRows === 0) {
+          res.status(404).send({ message: 'Échec de la mise à jour de l’Semencier' });
+          return;
+        }
+        res.status(200).json({ message: 'Semencier mis à jour avec succès' });
+      });
+    }
+  } catch (e) {
+    res.status(500).send({erreur: e})
+  }
 };
 
 // Supprimer un Semencier
