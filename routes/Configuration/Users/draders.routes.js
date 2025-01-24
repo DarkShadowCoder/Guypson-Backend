32732ffd2e19668ec213e29987332d7975f94200
Blackdot <106665251+DarkShadowCoder@users.noreply.github.com>
2025-01-24 16:30:45 +0100

    Update draders.routes.js

--- routes/Configuration/Users/draders.routes.js
+++ routes/Configuration/Users/draders.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const DraderController = require('../../../controllers/Users/draders.controller');
+const DraderController = require('../../../controllers/Configuration/Users/draders.controller');
 
 // Routes Producteurs
 router.get('/', DraderController.getAllDraders);
@@ -8,5 +8,8 @@ router.get('/:id', DraderController.getDraderById);
 router.post('/', DraderController.createDrader);
 router.put('/:id', DraderController.updateDrader);
 router.delete('/:id', DraderController.deleteDrader);
+
+// Routes de listing
+router.get('/ddaders/:id', DraderController.getDdaders);
 
 module.exports = router;
